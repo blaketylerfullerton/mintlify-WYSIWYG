b84acc015c2c04fc258e01682b6596b3fd23cf2e
Blake Fullerton <fullerton199@gmail.com>
2025-08-07 18:36:03 -0700

    adding logo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -25,6 +25,13 @@ export default function Layout({
           <div className="flex items-center gap-2">
             <h1 className="text-lg font-semibold">Blakes Mintlify </h1>
             <img src="/favicon.ico" alt="Blakes Mintlify" className="w-6 h-6" />
+            <a
+              href="https://github.com/blaketylerfullerton/mintlify-WYSIWYG"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/github.png" alt="Github" className="w-6 h-6" />
+            </a>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
